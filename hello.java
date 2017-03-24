c553f86af65908a849f54d8073c31dad5762e54f
d-mahantesh <mahantesh.dhale@unotechsoft.com>
2017-03-24 15:17:42 +0700

    new lines of code added

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class hello{
 	public static void main (String[] args){
 		System.out.println("Hello World");
+		int a,b;
+		a = 10;
+		b = 5;
+		int sum = a + b;
+		System.out.print(sum);
 	}
 }
